221b3b1e7a965fcefa59774fbf0482fc7758e8e4
Paulo Vieira <paullocostta86@gmail.com>
2022-10-12 20:57:46 -0300

    feat: Created checkbox component

---
+++ src/components/commons/Checkbox/index.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text } from '../Text'
+
+import { Checkbox, CheckboxProps } from './'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => {
+      return (
+        <div className="flex items-center gap-2">
+          {Story()}
+          <Text size="sm">Lembrar-me por 30 dias</Text>
+        </div>
+      )
+    }
+  ]
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {}
